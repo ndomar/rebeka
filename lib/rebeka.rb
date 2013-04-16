9f6ce08572e2025e150274d876218109749a994c
Omar Nada <ndomar.14@gmail.com>
2013-04-16 12:02:46 +0200

    adds a wrapper for main weka SMO functions

---
+++ lib/rebeka.rb
@@ -0,0 +1,48 @@
+## Ruby - weka wrapper
+
+require 'rjb'
+dir = "./weka.jar"
+Rjb::load(dir, jvmargs=["-Xmx1000M"])
+## the function takes in some trained model, exports it to .model file
+def to_model classifier, name
+	Rjb::import("weka.core.SerializationHelper").write(name + ".model", classifier)
+end
+## the function returns .model file with a certain name
+def get_model name
+	Rjb::import("weka.core.SerializationHelper").read(name + ".model")
+end
+
+## the function returns a trained classifier given instances as an input
+def create_classifier instances
+	classifier = Rjb::import("weka.classifiers.functions.SMO").new
+	classifier.buildClassifier(instances)
+	classifier
+end
+
+## the function reads a csv file and returns the created instances
+def to_instances instances_csv
+	instances_file = get_file instances_csv
+	csv_loader = Rjb::import("weka.core.converters.CSVLoader").new
+	csv_loader.setFile(instances_file)
+	instances = csv_loader.getDataSet()
+	instances.setClassIndex(instances.numAttributes() - 1)
+	instances
+end
+
+## the function evaluates a single instance given the classifier name and the instance, it returns a labeled
+## evaluated instance
+def evaluate_single_instance classifier, instance
+	label = classifier.classifyInstance(instance)
+	instance.setClassValue(label)
+	instance
+end
+
+## function returns a java object of the file with a certain name.
+def get_file name
+	Rjb::import("java.io.File").new(name)
+end
+
+## given an instance, it returns the class of this instance (class attribute should always be the last one.)
+def get_class instance
+	instance.toString.split(",")[instance.numAttributes() - 1]
+end
